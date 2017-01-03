c11a97566992cbe1f69f400e0d9960eb4da935af
misko <misko@miskodeMacBook-Pro.local>
2017-01-03 11:28:00 +0800

    first add

---
+++ lib.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+
+	"hash/crc64"
+	"os"
+
+	"github.com/google/btree"
+)
+
+type Item string
+
+type Iterator func(item string)
+
+func (s Item) Less(than btree.Item) bool {
+	if strings.Compare(string(s), string(than.(Item))) == -1 {
+		return true
+	}
+	return false
+}
+
+type MultiBtree struct {
+	maxBuffer uint32
+	groups    int
+	path      string
+	prefix    string
+	buffers   map[int]*os.File
+}
+
+func NewMultiTree(path string, groups int) (*MultiBtree, error) {
+	tree := new(MultiBtree)
+	tree.groups = groups
+	tree.buffers = make(map[int]*os.File, groups)
+	tree.path = path + "/tree_buffers/"
+	tree.prefix = "temp"
+
+	err := os.MkdirAll(tree.path, os.ModePerm)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < groups; i++ {
+		f, ferr := tree.openFile(i)
+
+		if ferr != nil {
+			return nil, ferr
+		}
+		tree.buffers[i] = f
+	}
+	return tree, nil
+}
+
+func (t *MultiBtree) Insert(item string) error {
+	group := strHash(item, t.groups)
+	_, err := t.buffers[group].Write([]byte(item + "\n"))
+	return err
+}
+
+func (t *MultiBtree) Scan(c Iterator) {
+	for _, f := range t.buffers {
+		f.Seek(0, 0)
+		tree := btree.New(4)
+		scanner := bufio.NewScanner(f)
+		buf := make([]byte, 1024)
+		scanner.Buffer(buf, 1024)
+		for scanner.Scan() {
+			tree.ReplaceOrInsert(Item(scanner.Text()))
+		}
+		tree.Ascend(func(item btree.Item) bool {
+			c(string(item.(Item)))
+			return true
+		})
+	}
+}
+
+func (t *MultiBtree) openFile(group int) (*os.File, error) {
+	file := fmt.Sprintf("%s/%s_%d.buffer", t.path, t.prefix, group)
+	return os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+}
+
+func (t *MultiBtree) Close() {
+	for _, f := range t.buffers {
+		f.Close()
+	}
+	fmt.Println("closed")
+	//os.RemoveAll(t.path)
+
+}
+
+func strHash(str string, max int) int {
+	crcTable := crc64.MakeTable(crc64.ECMA)
+	checksum64 := crc64.Checksum([]byte(str), crcTable)
+	return int(checksum64 % uint64(max))
+}
---
+++ lib_test.go
@@ -0,0 +1,69 @@
+package testing
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+var letterBytes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func genRandomStr() string {
+
+	perm := rand.Perm(len(letterBytes))
+	p := ""
+	for _, i := range perm[:20] {
+		p = p + string(letterBytes[i])
+	}
+	perm = rand.Perm(len(letterBytes))
+	for _, i := range perm[:20] {
+		p = p + string(letterBytes[i])
+	}
+	return p
+}
+
+func TestTree(t *testing.T) {
+
+	multiBtree, err := NewMultiTree("./test_files", 20)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer multiBtree.Close()
+
+	now := time.Now()
+
+	for i := 0; i < 20000; i++ {
+		err = multiBtree.Insert(genRandomStr())
+		if err != nil {
+			fmt.Println(err, "write")
+			return
+		}
+	}
+	fmt.Println(time.Now().Sub(now))
+
+	stats := &runtime.MemStats{}
+	runtime.ReadMemStats(stats)
+	b, _ := json.Marshal(stats)
+	fmt.Println("Last GC was:", string(b))
+
+	now = time.Now()
+
+	var c int = 0
+
+	multiBtree.Scan(func(item string) {
+		c++
+	})
+
+	runtime.ReadMemStats(stats)
+	b, _ = json.Marshal(stats)
+
+	fmt.Println(time.Now().Sub(now))
+	fmt.Println("Last GC was:", string(b))
+	fmt.Println(c)
+
+}
